2fe5e60fc3fbc4e9aeef6adc971a67d666c320d0
sfxdev <sfx.coding@gmail.com>
2016-12-24 15:15:02 -0600

    Basic starting code for pixi

--- js/game.js
+++ js/game.js
@@ -1,3 +1,12 @@
 /**
  * Created by keith on 12/24/16.
  */
+window.onload = function () {
+	var renderer = PIXI.autoDetectRenderer(512, 512);
+
+	document.getElementById("game").appendChild(renderer.view);
+
+	var stage = new PIXI.Container();
+
+	renderer.render(stage);
+}
